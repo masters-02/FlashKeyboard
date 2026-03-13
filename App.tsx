93858208d791fdf58f970969c7844f5ace3ae32d
masters-02 <chadeeepanethsara@gmail.com>
2026-03-13 08:57:27 +0530

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Flash Neon Keyboard Settings</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#0D0D0D',
+  },
+  text: {
+    color: '#FF007F',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
